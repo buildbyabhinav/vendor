71199e000ce8bddc93d4463837fee811e75ed090
buildbyabhinav <dhimanabhinav8295@gmail.com>
2025-10-24 03:34:25 +0530

    modelling of the backend api

---
+++ api/models/product.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+},{timestamps:true});
+
+const productSchema = new mongoose.Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  numReviews: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  price: {
+    type: number,
+    required: true,
+    default: 0,
+  },
+  countInStock: {
+    type: number,
+    required: true,
+    default: 0,
+  },
+  review:[reviewSchema]
+}, {timestamps:true});
+
+const Product = mongoose.Model('Product', productSchema);
+export default Product;
--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 import express from "express";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import connectDB from "./config/db.js";
 import products from "./data/product.js";
 
 dotenv.config();
 const port = process.env.PORT;
 
+connectDB();
 const app = express();
 
 app.get('/', (req,res) => {
