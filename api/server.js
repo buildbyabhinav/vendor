1b3679fc4ee6e424140486f7b9a45a8c4b0d31c0
buildbyabhinav <dhimanabhinav8295@gmail.com>
2025-11-01 06:58:18 +0530

    backend authentication setup

--- api/server.js
+++ api/server.js
@@ -1,17 +1,26 @@
 import express from "express";
 import dotenv from 'dotenv';
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+
 import  productRoutes from "./routes/productRoutes.js";
+import userRoutes from './routes/userRoutes.js'
+
 dotenv.config();
 const port = process.env.PORT;
 
 connectDB();
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser())
+
 app.get('/', (req,res) => {
     res.send('API is running')
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.listen(port, () => console.log('App is running'));
